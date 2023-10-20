6e152be64d690375fbe6dca8482facf64c147589
Bilal Nasir <bilalnasirkhan9@gmail.com>
2023-10-19 21:42:28 -1200

    I Don't known

--- script.js
+++ script.js
@@ -73,9 +73,9 @@ gsap.to("#links .links_href",{
 })
 
 gsap.from("#privacy_logo_img",{
-  transform: "rotate(45deg)",
-  scale: 1.1,
-  duration: 3,
+  transform: "rotate(10deg)",
+  scale: 1,
+  duration: 1,
   opacity: 0,
   scrollTrigger:{
     trigger: "#page6",
@@ -156,3 +156,110 @@ function videoPlayBtn() {
 nav_Animation()
 hero_Animation();
 videoPlayBtn();
+
+const goodnessCards = document.querySelectorAll('.goodness_card');
+const goodnessCont = document.querySelector('.goodness_cont');
+const cardCount = goodnessCards.length;
+const cardWidth = goodnessCards[0].offsetWidth;
+
+let currentCardIndex = 0;
+
+goodnessCards.forEach((card, index) => {
+    card.addEventListener('click', () => centerCard(index));
+});
+
+function centerCard(cardIndex) {
+    const leftDistance = (cardIndex - currentCardIndex + cardCount) % cardCount;
+    const rightDistance = (currentCardIndex - cardIndex + cardCount) % cardCount;
+
+    // Determine whether to scroll left or right based on the shortest distance.
+    if (leftDistance <= rightDistance) {
+        currentCardIndex = (currentCardIndex + leftDistance) % cardCount;
+    } else {
+        currentCardIndex = (currentCardIndex - rightDistance + cardCount) % cardCount;
+    }
+
+    scrollCards();
+}
+
+function scrollCards() {
+    const scrollLeft = currentCardIndex * cardWidth;
+    goodnessCont.scrollTo({
+        left: scrollLeft,
+        behavior: 'smooth',
+    });
+}
+
+function hidePlaceholder(input) {
+  input.placeholder = '';
+  var placeholderImage = document.querySelector('#placeholderImage')
+  placeholderImage.style.display = "none";
+}
+var animcommit = document.querySelectorAll(".bounding_h3 h3");
+var goodnessCard = document.querySelector("#card1");
+var goodnessCard2 = document.querySelector("#card2");
+var goodnessCard3 = document.querySelector("#card3");
+var goodnessCard4 = document.querySelector("#card4");
+var goodnessCard5 = document.querySelector("#card5");
+var heading = document.getElementsByClassName('.bound_h3_1');
+var circlecont = document.querySelector(".goodness_card")
+var circleanim = document.querySelector(".side_circle2")
+var animationTriggered = false; 
+
+circleanim.addEventListener("click", function(){
+  circleanim.style.backgroundColor = 'green'
+})
+
+
+function clientComments() {
+    gsap.from(animcommit, {
+      y: 150,
+      opacity: 0,
+      delay: 0.1, // Reduce the delay for a quicker animation
+      duration: 0.4,
+      stagger: 0.08,
+     
+    });
+  }
+
+goodnessCard.addEventListener("click", clientComments);
+goodnessCard2.addEventListener("click", clientComments);
+goodnessCard3.addEventListener("click", clientComments);
+goodnessCard4.addEventListener("click", clientComments);
+goodnessCard5.addEventListener("click", clientComments);
+
+
+card2.addEventListener("click", function() {
+  card1.heading.textContent = "Hello fucker"
+  heading.textContent = 'New text for Card 2';
+  clientComments(); 
+
+});
+
+card3.addEventListener("click", function() {
+  card2.heading.style.display = "none"
+
+  clientComments(); 
+
+});
+card4.addEventListener("click", function() {
+  card3.heading.style.display = "none"
+  clientComments(); // Run the animation
+
+});
+card5.addEventListener("click", function() {
+  card4.heading.style.display = "none"
+  clientComments(); // Run the animation
+
+});
+
+
+
+
+
+
+
+
+
+
+
